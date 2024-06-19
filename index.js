f65ffadd00e98f95a0ba7d99b651ba07bd425a6f
cobblesteve-dev <channelediting521@gmail.com>
2024-06-19 12:10:06 -0500

    did some more changelogging

--- index.js
+++ index.js
@@ -2,3 +2,10 @@ function duolingoAlert(){
     alert('Be Warned...');
     alert('This Is A Point Of No Return...');
 };
+
+function figureItOut(){
+    document.getElementById('figure-it-out').textContent = 'Whomp Whomp';
+    setTimeout(() => {
+        document.getElementById('figure-it-out').textContent = 'Or Just Make One?';
+    }, 2500)
+};
